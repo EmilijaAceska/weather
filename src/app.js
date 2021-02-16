8b1521f04821ef76196808f3a6132277a01beb3a
Emilija Aceska <69395075+EmilijaAceska@users.noreply.github.com>
2021-02-16 16:06:45 +0100

    display city,current date,description,degrees,humidity and wind

--- src/app.js
+++ src/app.js
@@ -1,4 +1,52 @@
-function dosplayWeather(response) {
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function displayWeather(response) {
+  console.log(response.data);
+  let displayCity = document.querySelector("#display-city");
+  displayCity.innerHTML = response.data.name;
+
+  let displayCurrentDate = document.querySelector("#current-date");
+  displayCurrentDate.innerHTML = formatDate(response.data.dt * 1000);
+
+  let displayCelsius = document.querySelector("#deg-celsius");
+  displayCelsius.innerHTML = Math.round(response.data.main.temp);
+
+  let displayDescription = document.querySelector("#description");
+  displayDescription.innerHTML = response.data.weather[0].description;
+
+  let displayHumidity = document.querySelector("#humidity");
+  displayHumidity.innerHTML = response.data.main.humidity;
+
+  let displayWind = document.querySelector("#wind");
+  displayWind.innerHTML = Math.round(response.data.wind.speed);
 
 }
 
